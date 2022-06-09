c3d905ee1c75e5298e86d3ee6610fff488622ad3
datGUUYY <trey.mckinstry@live.com>
2022-06-09 17:04:29 +0000

    Added worldvariables class. Added ineffective typechart, will be refactored

---
+++ idk/src/main/java/com/treydev/idk/WorldVariables.java
@@ -0,0 +1,16 @@
+package com.treydev.idk;
+
+public class WorldVariables {
+    /*
+     * This class includes variables genrated upon worldgen, such as the map and typecharts.
+     * It may also include some session variables like location, creatures discovered,
+     * and creatures in party/box.
+     */
+    private byte typeChart;
+    public WorldVariables()
+    {
+        //Generate typechart.
+        typeChart = 5; //TODO undo hard coding.
+    }
+
+}
