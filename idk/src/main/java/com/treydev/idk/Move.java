a9814e639f8461c0f779861ae568c903a624f5f6
datGUUYY <trey.mckinstry@live.com>
2022-06-12 22:01:49 +0000

    Added AllMoves static member in Move class, implemented getRandomMove method.

--- idk/src/main/java/com/treydev/idk/Move.java
+++ idk/src/main/java/com/treydev/idk/Move.java
@@ -1,12 +1,33 @@
 package com.treydev.idk;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public abstract class Move {
 
     //TODO: Implement "type" (Status, Physical, Special) as subclasses. 
-    //  P & S: "Execute" function runs private "Damage" function, then call "Effect"
+    //  P & Sp: "Execute" function runs private "Damage" function, then call "Effect"
+    //  Status can just be implemented as a base move.
     public String name;
+    private static Random random;
+
+    private static Move[] AllMoves = {
+        new PhysicalAttack("Just hit it.", 15, 80)
+        {
+            @Override 
+            protected void AddEffects(ArrayList<String> outputs, Critter Target, Critter user) {}
+        },
+        new PhysicalAttack("Hit it harder!", 30, 40)
+        {
+            @Override 
+            protected void AddEffects(ArrayList<String> outputs, Critter Target, Critter user) {}
+        }
+    };
+
+    public static void initializeRandom(long seed)
+    {
+        random = new Random(seed);
+    }
 
     public abstract void Execute(ArrayList<String> outputs, Critter target, Critter user);
     
@@ -18,16 +39,13 @@ public abstract class Move {
     public static Move[] implementStubbedMoveList()
     {
         Move[] value = new Move[4];
-        value[0] = new PhysicalAttack("Just hit it.", 15, 80)
-        {
-            @Override 
-            protected void AddEffects(ArrayList<String> outputs, Critter Target, Critter user) {}
-        };
-        value[1] = new PhysicalAttack("Hit it harder!", 30, 40)
-        {
-            @Override 
-            protected void AddEffects(ArrayList<String> outputs, Critter Target, Critter user) {}
-        };
+        value[0] = AllMoves[0];
+        value[1] = AllMoves[1];
         return value;
     }
+    public static Move getRandomMove()
+    {
+        return AllMoves[random.nextInt(AllMoves.length)];
+    }
+
 }
