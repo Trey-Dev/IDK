9784ec79d3287fc58b0a1103be57a89c35ae528d
datGUUYY <trey.mckinstry@live.com>
2022-06-14 18:51:14 +0000

    cleaned up comments

--- idk/src/main/java/com/treydev/idk/Move.java
+++ idk/src/main/java/com/treydev/idk/Move.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public abstract class Move {
 
-    //TODO: Implement "type" (Status, Physical, Special) as subclasses. 
+    
     //  P & Sp: "Execute" function runs private "Damage" function, then call "Effect"
     //  Status can just be implemented as a base move.
     public String name;
@@ -14,18 +14,6 @@ public abstract class Move {
     private static ArrayList<Move> allMoves;
 
     //TODO: Generate something that creates a random move
-    // private static Move[] AllMoves = {
-    //     new PhysicalAttack("Just hit it.", 15, 80)
-    //     {
-    //         @Override 
-    //         protected void AddEffects(ArrayList<String> outputs, Critter Target, Critter user) {}
-    //     },
-    //     new PhysicalAttack("Hit it harder!", 30, 40)
-    //     {
-    //         @Override 
-    //         protected void AddEffects(ArrayList<String> outputs, Critter Target, Critter user) {}
-    //     }
-    // };
 
     static{
         long seed = 0; //TODO: remove hardcoded stub!
