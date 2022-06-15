d8cad7b0d92861e98e31b42ba1ba2c150cbe7489
datGUUYY <trey.mckinstry@live.com>
2022-06-15 03:24:59 +0000

    Added a function to generate moveset for critters

--- idk/src/main/java/com/treydev/idk/Move.java
+++ idk/src/main/java/com/treydev/idk/Move.java
@@ -28,10 +28,33 @@ public abstract class Move {
         allMoves.add(new PhysicalAttack("Don't hit it?", null, 123, 0));
         allMoves.add(new PhysicalAttack("Really risky, don't try it.", null, 80, 10));
     }
-
-    //TODO: something that creates a (stubbed) movelist
     //TODO: something that generates a nonstubbed moveset
 
+
+    public static Move []genMoveset(int level, HashMap<Integer,Move> moveList)
+    {
+        Move []value = new Move[4];
+        for(int i = 0; i < 4; i++)
+        {
+            int currentMoveLevel;
+            Boolean canContinue;
+            //fill out a single move.
+            do{
+                canContinue = true;
+                currentMoveLevel = getMoveInList(moveList);
+                for(Move valueMove : value)
+                {
+                    if(moveList.get(currentMoveLevel) == valueMove)
+                    {
+                        canContinue = false;
+                        break;
+                    }
+                }
+            } while(currentMoveLevel > level || !canContinue);
+            value[i] = moveList.get(currentMoveLevel);
+        }
+        return value;
+    }
     public abstract void Execute(ArrayList<String> outputs, Critter target, Critter user);
     
     public Move(String name, Element Movetype)
@@ -39,7 +62,6 @@ public abstract class Move {
         this.name = name;
         this.moveType = Movetype;
     }
-
     public static Move[] implementStubbedMoveList()
     {
         Move[] value = new Move[4];
@@ -52,6 +74,13 @@ public abstract class Move {
         //TODO: Implement method that either gets or generates a random move.
         return allMoves.get(random.nextInt(allMoves.size()));
     }
+    public static int getMoveInList(HashMap<Integer, Move> movelist)
+    {
+        int value = 0;
+        while(movelist.get(value) == null)
+            value = random.nextInt(100);
+        return value;
+    }
 
 
     public static HashMap<Integer,Move> genMoveList()
@@ -62,6 +91,7 @@ public abstract class Move {
             {
                 value.put(random.nextInt(100), Move.getRandomMove()); //TODO: Make sure no moves can be obtained for lower/higher level than evolutions!
                 System.out.println(value.keySet().toString());
+                //TODO: check for collisions!
             }
         return value;
     }
--- idk/src/test/java/com/treydev/idk/MoveTests.java
+++ idk/src/test/java/com/treydev/idk/MoveTests.java
@@ -30,6 +30,21 @@ public class MoveTests {
     }
 
     @Test
+    void testGenMoveset()
+    {
+        Move []sampleMoveset = Move.genMoveset(75, Move.genMoveList());
+        for(Move move : sampleMoveset)
+        {
+            ArrayList<Critter> StartAParty = Critter.initializeStubbedParty();
+            System.out.println("!!!!<<=================>>!!!!");
+            ArrayList<String> outputStrings = new ArrayList<>();
+            move.Execute(outputStrings, StartAParty.get(0), StartAParty.get(1));
+            for(String outputString : outputStrings)
+                System.out.println(outputString);
+        }
+    }
+
+    @Test
     void testGetRandomMove() {
         System.out.println("vvvvvvvvvvv TESTING GETRANDOMMOVE vvvvvvvvvvvvv");
         ArrayList<String> sampleOutputs = new ArrayList<String>();
