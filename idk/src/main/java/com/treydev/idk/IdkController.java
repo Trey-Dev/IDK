e5119b96f80a7291b8d805e77a1558af5ef2c49b
Dave McKinstry <dmckinstry@github.com>
2022-06-16 02:42:29 +0000

    Basic session handling for name and city

--- idk/src/main/java/com/treydev/idk/IdkController.java
+++ idk/src/main/java/com/treydev/idk/IdkController.java
@@ -6,20 +6,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.servlet.http.HttpSession;
+import com.treydev.idk.location.*;
 
 @Controller
 public class IdkController {
-    //@RequestMapping(value="/")
-    //@ResponseBody
+
     @GetMapping("/")
-    public String helloWorld(Model model)
+    public String login(Model model)
     {
-        return "MainPage";
-        //TODO: STORE PARAMETER Uname INTO SESSION VARIABLES
+        return "Login";
     }
 
+    @PostMapping("Login")
+    public String login(@RequestParam("name") String name, HttpSession session, Model model) {
+        model.addAttribute("name", name);
+        session.setAttribute("name", name);
+        return "City";
+    }
+
+    @GetMapping("Game")
     @PostMapping("Game")
-    public String game(@RequestParam("name") String name, @RequestParam("action") String action, Model model)
+    public String game(@RequestParam("action") String action, Model model, HttpSession session)
     {
         ArrayList<String> gameLog = new ArrayList<String>();
         gameLog.add("LineOne");
@@ -34,9 +42,10 @@ public class IdkController {
         actionList.add("Action 3");
         actionList.add("Action 4");
 
+        model.addAttribute("name", session.getAttribute("name"));
         model.addAttribute("gameLog", gameLog);
         model.addAttribute("actionList", actionList);
-        model.addAttribute("name", name);
+        model.addAttribute("name", session.getAttribute("name"));
 
         return "Game";
     }
@@ -46,28 +55,30 @@ public class IdkController {
     public static ArrayList<Critter> PlayerParty;
 
     @GetMapping("Inventory")
-    public String menu(Model model)
+    public String menu(Model model, HttpSession session)
     {
-       model.addAttribute("TableName", "Inventory");
-       if(Item.inventory == null)
-       {
+        model.addAttribute("name", session.getAttribute("name"));
+        model.addAttribute("TableName", "Inventory");
+        if(Item.inventory == null)
+        {
             Item.inventory = new ArrayList<Item>();
             Item.inventory.add(new Item("Red Potion", (short) 15, "Smells kinda fishy"));
             Item.inventory.add(new Item("Pink Potion", (short) 11, "Smells kinda beefy"));
-       }
-       model.addAttribute("Table", Item.inventory);
+        }
+        model.addAttribute("Table", Item.inventory);
         return "Inventory";
     }
 
     @PostMapping("Inventory")
-    public String consume(@RequestParam("name") String name,Model model)
+    public String consume(@RequestParam("name") String name,Model model, HttpSession session)
     {
-       model.addAttribute("TableName", "Inventory");
-       Item.InitializeStubbedInventory();
+        model.addAttribute("name", session.getAttribute("name"));
+        model.addAttribute("TableName", "Inventory");
+        Item.InitializeStubbedInventory();
 
-       String output = "";
-       if(!name.isEmpty())
-           output += Item.consume(name);
+        String output = "";
+        if(!name.isEmpty())
+            output += Item.consume(name);
 
         output += "\nWhat would you like to do?";
         model.addAttribute("output", output);
@@ -76,7 +87,7 @@ public class IdkController {
     }
 
     @GetMapping("EnterCombat")
-    public String combat(Model model)
+    public String combat(Model model, HttpSession session)
     {
         if(PlayerParty == null)
             {
@@ -88,24 +99,64 @@ public class IdkController {
             }
         //sessionCombatHandler.updateCombatOptions();
        
+        model.addAttribute("name", session.getAttribute("name"));
         model.addAttribute("CombatOutput", sessionCombatHandler.outputs);
         model.addAttribute("FightOptions", sessionCombatHandler.CombatOptions);
         return "Combat";
     }
     
     @PostMapping("EnterCombat")
-    public String combat(@RequestParam("Option") String Option, Model model)
+    public String combat(@RequestParam("Option") String Option, Model model, HttpSession session)
     {
         sessionCombatHandler.takeAction(Option);
-       model.addAttribute("CombatOutput", sessionCombatHandler.outputs);
-       model.addAttribute("FightOptions", sessionCombatHandler.CombatOptions);
+        model.addAttribute("CombatOutput", sessionCombatHandler.outputs);
+        model.addAttribute("FightOptions", sessionCombatHandler.CombatOptions);
+        model.addAttribute("name", session.getAttribute("name"));
 
         return "Combat";
     }
     
     @GetMapping("City")
-    public String city(Model model)
+    public String city(Model model, HttpSession session)
     {
+        if (session.getAttribute("city") == null)
+        {
+            City c = City.getCityByLevel(1);
+            session.setAttribute("city", c);
+        }
+
+        model.addAttribute("name", session.getAttribute("name"));
+        City c = (City)session.getAttribute("city");
+        model.addAttribute("cityName", c.getName());
+
+        // Extract names for Shops, Gyms and Paths and place into sessino arrays
+        // TO DO: This code is excessive and should be streamlined/refactored
+        String[] shops = new String[c.getShops().size()];
+        for(int i = 0; i < c.getShops().size(); i++)
+        {
+            shops[i] = c.getShops().get(i).getName();
+        }
+        model.addAttribute("shops", shops);
+
+        String[] gyms = new String[c.getGyms().size()];
+        for(int i = 0; i < c.getShops().size(); i++)
+        {
+            gyms[i] = c.getGyms().get(i).getName();
+        }
+        model.addAttribute("gyms", gyms);
+
+        // STUB
+        model.addAttribute("paths", new String[]{"Path 1", "Path 2", "Path 3"});
+
+        dumpModel(model);
         return "City";
+    }
+
+    private void dumpModel(Model model)
+    {
+        for(String key : model.asMap().keySet())
+        {
+            System.out.println(key + ": " + model.asMap().get(key));
+        }
     }
 }
