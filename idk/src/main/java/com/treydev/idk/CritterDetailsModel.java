db9f30b441a2c96904c832164b1d81b5546331b2
datGUUYY <trey.mckinstry@live.com>
2022-06-17 20:42:36 +0000

    Responded to comments, cleaned up template/code

--- idk/src/main/java/com/treydev/idk/CritterDetailsModel.java
+++ idk/src/main/java/com/treydev/idk/CritterDetailsModel.java
@@ -1,12 +1,28 @@
 package com.treydev.idk;
 
 public class CritterDetailsModel {
-    public String  D, SA, SD, SPD, HP;
-    public String A = "";
+    public class statLine {
+        public String attributeName;
+        public String attributeValue;
+        public String attributeColor;
+        public statLine(String name, String value, String color)
+        {
+            this.attributeName = name;
+            this.attributeValue = value;
+            this.attributeColor = color;
+        }
+    }
+
+    // public String  A, D, SA, SD, SPD, HP;
+    // public String A = "";
     public String name, SpeciesName;
+    public final String[] statNames = {"Attack", "Defense", "Special Attack", "Special Defense", "Speed", "Hit Points"};
+    public final String[] colors={"Red", "DarkGrey", "DarkMagenta", "Blue", "Orange", "ForestGreen"};
+    public statLine []statValues;
     //TODO: when ability is implemented, add ability details!
     public CritterDetailsModel(Critter C)
     {
+        String  A, D, SA, SD, SPD, HP;
         A = D = SA = SD = SPD = HP = "";
         int Atk = C.species.getBaseAttack();//TODO: WHEN THIS MERGES, IMPLEMENT ACTUAL STAT AQUIRATION.
         for(int i = 0; i < Atk; i++)
@@ -34,6 +50,10 @@ public class CritterDetailsModel {
         HP += " HP: " + HiP; //TODO: normalize HP so it doesn't take up more than every other stat.
         //TODO: MAYBE replace i++ with i+= 2, scale, then add a half box if i%2==1.
         //      OR simply decrease the font size, maybe fit 6 on a screen!
+        String[] s =  {A,D,SA,SD,SPD,HP};
+        statValues = new statLine[6];
+        for(int i = 0; i < s.length; i++)
+            statValues[i] = new statLine(statNames[i], s[i], colors[i]);
         name = C.name;
         SpeciesName = C.species.name;
     }
