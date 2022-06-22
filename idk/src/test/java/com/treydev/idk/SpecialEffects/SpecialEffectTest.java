f562de3feaa620e7ec7aa096c5b80900eca69270
datGUUYY <trey.mckinstry@live.com>
2022-06-22 18:25:07 +0000

    Added basic comments for test, outlining how test is created.

--- idk/src/test/java/com/treydev/idk/SpecialEffects/SpecialEffectTest.java
+++ idk/src/test/java/com/treydev/idk/SpecialEffects/SpecialEffectTest.java
@@ -6,5 +6,8 @@ public class SpecialEffectTest {
     @Test
     void testActivate() {
         //Test activate with a stubbed special effect
+        //Effect should not activate when the effect evaluator returns false
+        //Effect should activate when the effect evaluator returns true
+        //Effect should execute the effect execution when the effect evaluator returns true.
     }
 }
